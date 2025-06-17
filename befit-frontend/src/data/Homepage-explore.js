06ad4014b17ceb3bae99043cac41f51f68bd4c44
jaishruti <shrutijaiswal223@gmail.com>
2025-06-17 16:32:02 +0530

    data for explore more tab

---
+++ befit-frontend/src/data/Homepage-explore.js
@@ -0,0 +1,80 @@
+export const HomePageExplore = [
+  {
+    tags: "Yoga",
+    courses: [
+      {
+        heading: "Practise Yoga",
+        description:
+          "This course offers basics of yoga and asanas, posture correction and best practices for well being",
+        level: "beginner",
+        sessionNumber: 10,
+      },
+      {
+        heading: "Practise Yoga",
+        description:
+          "This course offers basics of yoga and asanas, posture correction and best practices for well being",
+        level: "beginner",
+        sessionNumber: 10,
+      },
+      {
+        heading: "Practise Yoga",
+        description:
+          "This course offers basics of yoga and asanas, posture correction and best practices for well being",
+        level: "beginner",
+        sessionNumber: 10,
+      },
+    ],
+  },
+  {
+    tags: "Medidation",
+    courses: [
+      {
+        heading: "Practise Yoga",
+        description:
+          "This course offers basics of yoga and asanas, posture correction and best practices for well being",
+        level: "beginner",
+        sessionNumber: 10,
+      },
+      {
+        heading: "Practise Yoga",
+        description:
+          "This course offers basics of yoga and asanas, posture correction and best practices for well being",
+        level: "beginner",
+        sessionNumber: 10,
+      },
+      {
+        heading: "Practise Yoga",
+        description:
+          "This course offers basics of yoga and asanas, posture correction and best practices for well being",
+        level: "beginner",
+        sessionNumber: 10,
+      },
+    ],
+  },
+  {
+    tags: "Gym",
+    courses: [
+      {
+        heading: "Practise Yoga",
+        description:
+          "This course offers basics of yoga and asanas, posture correction and best practices for well being",
+        level: "beginner",
+        sessionNumber: 10,
+      },
+      {
+        heading: "Practise Yoga",
+        description:
+          "This course offers basics of yoga and asanas, posture correction and best practices for well being",
+        level: "beginner",
+        sessionNumber: 10,
+      },
+      {
+        heading: "Practise Yoga",
+        description:
+          "This course offers basics of yoga and asanas, posture correction and best practices for well being",
+        level: "beginner",
+        sessionNumber: 10,
+      },
+    ],
+  },
+];
