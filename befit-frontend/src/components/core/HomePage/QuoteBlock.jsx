5ca680d2b3a46aa8777ab0da5984feb4ef3fae5c
jaishruti <shrutijaiswal223@gmail.com>
2025-06-17 12:50:03 +0530

    Quoteblock component

--- befit-frontend/src/components/core/HomePage/QuoteBlock.jsx
+++ befit-frontend/src/components/core/HomePage/QuoteBlock.jsx
@@ -9,26 +9,58 @@ const QuoteBlock = ({
   ctabtn1,
   ctabtn2,
   codeblock,
-  bgradient,
   codecolor,
 }) => {
   return (
-    <div className={`flex ${position} my-20 gap-5`}>
+    <div className={`flex ${position} my-20 gap-5 justify-between`}>
       {/* section1 */}
-      <div>
-        <div>
-          <div></div>
-        </div>
-        <div>
-          <CTAButton></CTAButton>
-          <CTAButton></CTAButton>
+      <div className="flex flex-col w-[50%] gap-8">
+        {heading}
+        <div className="text-slate-800 font-bold">{subheading}</div>
+        <div className="flex gap-7 mt-7">
+          <CTAButton active={ctabtn1.active} linkto={ctabtn1.linkto}>
+            <div className="flex gap-2 items-center">
+              {ctabtn1.btnText}
+              <FaArrowRight />
+            </div>
+          </CTAButton>
+          <CTAButton active={ctabtn2.active} linkto={ctabtn2.linkto}>
+            {ctabtn2.btnText}
+          </CTAButton>
         </div>
       </div>
       {/* section2 */}
-      <div>
-        <div></div>
-        <div></div>
+      <div className="h-fit  flex flex-row text-10[px] w-[100%] py-4 lg:w-[500px]">
+        <div className="text-center flex flex-col w-[10%] text-richblack-400 font-inter font-bold">
+          <p>1</p>
+          <p>2</p>
+          <p>3</p>
+          <p>4</p>
+          <p>5</p>
+          <p>6</p>
+          <p>7</p>
+          <p>8</p>
+          <p>9</p>
+          <p>10</p>
+          <p>11</p>
+        </div>
+        <div
+          className={`w-[90%] flex flex-col gap-2 font-bold font-mono ${codecolor} pr-2`}
+        >
+          <TypeAnimation
+            sequnece={[codeblock, 2000, ""]}
+            repeat={Infinity}
+            cursor={true}
+            style={{
+              whiteSpace: "pre-line",
+              display: "block",
+            }}
+            omitDeletionAnimation={true}
+          />
+        </div>
       </div>
     </div>
   );
 };
+
+export default QuoteBlock;
