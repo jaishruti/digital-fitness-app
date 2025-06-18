db90b70a82ff64279dff016e45a2fa264adfc60b
jaishruti <shrutijaiswal223@gmail.com>
2025-06-18 12:37:49 +0530

    review data

---
+++ befit-frontend/src/data/Review-data.js
@@ -0,0 +1,43 @@
+import Instructor from "../assets/Images/Instructor.png";
+const ReviewData = [
+  {
+    image: { Instructor },
+    name: "Medha",
+    location: "Pune",
+    description: "Unique experience at BeFit",
+    rating: "4.3",
+    star: 5,
+  },
+  {
+    image: { Instructor },
+    name: "Niti",
+    location: "Pune",
+    description: "Unique experience at BeFit",
+    rating: "4.3",
+    star: 4,
+  },
+  {
+    image: { Instructor },
+    name: "Richa",
+    location: "Pune",
+    description: "Unique experience at BeFit",
+    rating: "4.3",
+    star: 3,
+  },
+  {
+    image: { Instructor },
+    name: "Vaidya",
+    location: "Pune",
+    description: "Unique experience at BeFit",
+    rating: "4.3",
+    star: 4,
+  },
+  {
+    image: { Instructor },
+    name: "Richa",
+    location: "Pune",
+    description: "Unique experience at BeFit",
+    rating: "4.3",
+    star: 5,
+  },
+];
