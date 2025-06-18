2db9280cc79a68945cda0d60a9539c739ff68ae4
jaishruti <shrutijaiswal223@gmail.com>
2025-06-18 14:08:42 +0530

    color add

--- befit-frontend/src/components/core/HomePage/Button.jsx
+++ befit-frontend/src/components/core/HomePage/Button.jsx
@@ -6,7 +6,7 @@ const CTAButton = ({ children, active, linkto }) => {
       <div
         className={`max-w-xs py-3 px-3 text-center text-black rounded-md font-semibold
       shadow-md hover:bg-gray-300 hover:scale-95 focus:ring-2 focus:ring-blue-400 transition-all duration-200 
-  ${active ? "bg-amber-400 text-black" : "border-0 border-black"}`}
+  ${active ? "bg-amber-400 text-black" : "border-0 border-black bg-white"}`}
       >
         {children}
       </div>
